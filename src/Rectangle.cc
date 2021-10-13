694b0b0d33291b06a84a8e5061aa64fa9e687377
AAIE02 <zap592@amerike.edu.mx>
2021-10-12 22:44:41 -0500

    V1

--- src/Rectangle.cc
+++ src/Rectangle.cc
@@ -2,9 +2,9 @@
 
 Rectangle::Rectangle(int w, int h, int x, int y, sf::Color color)
 {
-    rectangleShape = new sf::RectangleShape(sf::Vector2f(w, h));
-    rectangleShape->setPosition(sf::Vector2f(x,y));
-    rectangleShape->setFillColor(color);
+  rectangleShape = new sf::RectangleShape(sf::Vector2f(w, h));
+  rectangleShape->setPosition(sf::Vector2f(x, y));
+  rectangleShape->setFillColor(color);
 }
 
 Rectangle::~Rectangle()
@@ -13,5 +13,5 @@ Rectangle::~Rectangle()
 
 sf::RectangleShape* Rectangle::GetShape() const
 {
-    return rectangleShape;
+  return rectangleShape;
 }
